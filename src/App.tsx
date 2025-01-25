adc7e6afe0ca70a74bb21a1ecec584c296a6987a
Ruveyda Kisla <thisisruveyda@gmail.com>
2025-01-26 00:21:01 +0330

    feat: add comming soon

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="center bg-[#FFCDCD] h-screen w-screen">
+      <a href="https://git.io/typing-svg">
+        <img
+          src="https://readme-typing-svg.demolab.com?font=JetBrains+Mono&weight=500&size=56&duration=2000&pause=1000&color=0D1292&center=true&vCenter=true&width=1200&height=300&lines=+Secure%2C+Simple+and+Powerful.;Comming+soon;BLUX+CC"
+          alt="Typing SVG"
+        />
+      </a>
+    </div>
+  );
+}
+
+export default App;
