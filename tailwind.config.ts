91998997747bb5672e70501422031088e577e281
Ruveyda Kisla <thisisruveyda@gmail.com>
2025-04-08 18:05:17 +0330

    fix: add max-width

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
       screens: {
         short: { raw: "(min-width: 1500px) and (max-height: 900px)" },
         tall: { raw: "(min-width: 1500px) and (min-height: 901px)" },
-        mobile: { max: "770px" },
+        mobile: { max: "771px" },
         tablet: { min: "771px", max: "1297px" },
         desktop: { min: "1298px" },
       },
