29824b850c77f69c8211a6d375f95259fdb77ce5
Ruveyda Kisla <thisisruveyda@gmail.com>
2025-04-19 20:57:25 +0330

    feat: add SVG logos and update App component for social links

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import { useState } from "react";
+
 import blux from "/blux.svg";
 import stellar from "/stellar.svg";
 import whiteArrow from "/whiteArrow.svg";
-import blueArrow from "/blueArrow.svg";
 import meadow from "/meadow.svg";
 import copyIcon from "/copy.svg";
 import bluxProduct from "/bluxProduct.svg";
-import { useState } from "react";
 
 function App() {
   const [copied, setCopied] = useState(false);
@@ -22,27 +22,48 @@ function App() {
       className="flex font-manrope flex-col justify-between items-center !bg-white font-medium
     text-primary py-[17px] tablet:px-10 tablet:py-8 mobile:px-6 h-screen w-screen desktop:max-h-[1000px]"
     >
-      <div className="h-[114px] tablet:h-[74px] mobile:h-auto flex w-full justify-between items-end py-[18px] mobile:pt-0 mobile:pb-3 max-w-[1280px] short:max-w-[1210px]">
+      <div className="h-[114px] mobile:flex-col mobile:items-start mobile:gap-6 tablet:h-[74px] mobile:h-auto flex w-full justify-between items-end py-[18px] mobile:pt-0 mobile:pb-3 max-w-[1280px] short:max-w-[1210px]">
         <img
           src={blux}
           alt="blux"
           className="desktop:w-[361px] desktop-h-[78px] tablet:w-[236px] tablet-h-[51px] mobile:w-[268px] mobile-h-[58px]"
         />
-        <div className="flex gap-4 tablet:text-xs mobile:hidden">
+        <div className="flex gap-4 mobile:gap-[9px] tablet:text-xs mobile:flex-row-reverse">
           <a
-            className="cursor-pointer center h-12 tablet:h-8 center desktop:w-[143px] tablet:w-[100px] hover:bg-[#F2F2F2] transition-colors duration-500"
+            className="cursor-pointer center h-12 mobile:h-10 tablet:h-8 center desktop:w-[143px] tablet:w-[100px] hover:bg-[#F2F2F2] transition-colors duration-500"
             href="https://docs.blux.cc/"
             target="_blank"
           >
             Documentation
           </a>
           <a
-            className="border cursor-pointer border-primary desktop:w-[129px] tablet:w-[95px] gap-1 h-12 tablet:h-8 center hover:bg-[#E8E8FD] transition-colors duration-500"
+            className="size-12 tablet:size-8 mobile:size-10 border border-[#CDCEEE] center hover:bg-[#F2F2F2] transition-colors duration-500"
+            href="https://x.com/bluxofficial"
+            target="_blank"
+          >
+            <img src="/public/XLogo.svg" alt="X" height={24} width={24} />
+          </a>
+          <a
+            className="size-12 tablet:size-8 mobile:size-10 border border-[#CDCEEE] center hover:bg-[#F2F2F2] transition-colors duration-500"
+            href="https://discord.gg/PyDjPnWUYC"
+            target="_blank"
+          >
+            <img
+              src="/public/discordLogo.svg"
+              alt="discord"
+              height={24}
+              width={24}
+            />
+          </a>
+          <a
+            className="border mobile:hidden text-white cursor-pointer bg-primary
+            hover:bg-[#04062F] desktop:w-[129px] tablet:w-[95px] gap-1 h-12
+            tablet:h-8 flex justify-center items-center transition-colors duration-500 "
             href="https://demo.blux.cc/"
             target="_blank"
           >
             Get Demo
-            <img src={blueArrow} alt="arrow" className="tablet:size-3" />
+            <img src={whiteArrow} alt="arrow" className="tablet:size-3" />
           </a>
         </div>
       </div>
